7d4785edf18548fa43a8b6e0cb61d2c42d6654f5
AnastassiaKostjuk <anastassia.kostjuk@gmail.com>
2025-01-30 12:01:38 +0200

    Update localhost_film.sql

--- localhost_film.sql
+++ localhost_film.sql
@@ -3,3 +3,6 @@ filmID int PRIMARY KEY AUTO_INCREMENT,
 filmNimetus varchar(30) not null,
 aasta int,
 eelarveHind decimal(7,1));
+
+INSERT INTO film(filmNimetus, aasta, eelarveHind)
+VALUES('', 2024, 555555.5)
